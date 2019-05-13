fc410df01c11bf06104002fa1692a87aaa8c32c3
Tal Luigi <luigi.t.agr@gmail.com>
2019-05-13 11:00:18 -0400

    added candies sub-reducer to manage candies in my redux store

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,47 @@
+import axios from 'axios';
+import { combineReducers } from 'redux';
 
-const initialState = {}
+export const initialState = { candies: [] };
 
-const rootReducer = (state = initialState, action) => {
+export const SET_CANDIES = 'SET_CANDIES';
+
+export const setCandiesActionCreator = candies => ({
+  type: SET_CANDIES,
+  candies,
+});
+
+export const candiesReducer = (prevState = [], action) => {
+  switch (action.type) {
+    case SET_CANDIES:
+      const newState = [...action.candies];
+      return newState;
+    default:
+      return prevState;
+  }
+};
+
+export const rootReducer = (prevState = initialState, action) => {
+  const newState = JSON.parse(JSON.stringify(prevState));
   switch (action.type) {
     default:
-      return state
+      return prevState;
   }
-}
+};
 
-export default rootReducer
+export const getCandiesThunkCreator = () => {
+  return async dispatch => {
+    try {
+      const { data } = await axios.get('/api/candies');
+      dispatch(setCandiesActionCreator(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+const combinedReducers = combineReducers({
+  root: rootReducer,
+  candies: candiesReducer,
+});
+
+export default combinedReducers;
